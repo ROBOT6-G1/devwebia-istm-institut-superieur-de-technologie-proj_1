220b0f184748e3a11c5bc2b218580d7ff6efa308
ROBOT6-G1 <b.outiquemevasoa@gmail.com>
2026-07-23 19:38:47 +0300

    DEVWEBIA update sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'istm-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './script.js',
+  './firebase.js',
+  'https://unpkg.com/@tailwindcss/browser@4',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
